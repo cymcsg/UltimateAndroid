7579489f3e07cb4f7accdbb86b910cc4ac665100
marshalchen <cymcsg@gmail.com>
2014-07-24 10:17:03 +0800

    add swiped cards

---
+++ CommonApp/src/com/fss/Common/uiModule/cardsSwiped/view/BaseCardStackAdapter.java
@@ -0,0 +1,11 @@
+package com.fss.Common.uiModule.cardsSwiped.view;
+
+import android.widget.BaseAdapter;
+
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public abstract class BaseCardStackAdapter extends BaseAdapter {
+
+}
