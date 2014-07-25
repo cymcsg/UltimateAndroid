05b30c76bfacc0005b8834f84bc78574529f67fa
marshalchen <cymcsg@gmail.com>
2014-07-25 17:56:54 +0800

    update PackAgeUtils

--- CommonApp/src/com/fss/Common/commonUtils/basicUtils/PackageUtils.java
+++ CommonApp/src/com/fss/Common/commonUtils/basicUtils/PackageUtils.java
@@ -1,6 +1,8 @@
 package com.fss.Common.commonUtils.basicUtils;
 
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 
@@ -100,6 +102,16 @@ public class PackageUtils {
         }
         is.close();
         os.close();
+    }
+
+    public static String getVersionName(Context context) throws Exception {
+        // 获取packagemanager的实例
+        PackageManager packageManager = context.getPackageManager();
+        // getPackageName()是你当前类的包名，0代表是获取版本信息
+        PackageInfo packInfo = packageManager.getPackageInfo(context
+                .getPackageName(), 0);
+        String version = packInfo.versionName;
+        return version;
     }
 
 }
