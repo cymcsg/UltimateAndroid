9f737b3f495c21fa260d8423b1a7c980f8509a0f
marshalchen <cymcsg@gmail.com>
2014-07-25 09:21:30 +0800

    add list buddies

---
+++ CommonApp/src/com/fss/Common/uiModule/listbuddies/provider/ScrollConfigOptions.java
@@ -0,0 +1,14 @@
+package com.fss.Common.uiModule.listbuddies.provider;
+
+/**
+ * Created by jpardogo on 23/02/2014.
+ */
+public enum ScrollConfigOptions {
+    RIGHT,
+    LEFT;
+
+
+    public int getConfigValue() {
+        return ordinal() + 1;
+    }
+}
