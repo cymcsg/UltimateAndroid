97360a9c6836b99e6037308cdc927fba32994118
marshalchen <cymcsg@gmail.com>
2014-08-17 10:38:51 +0800

    add circular progress button

---
+++ CommonApp/src/com/fss/common/uiModule/circularProgressButton/OnAnimationEndListener.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2014. Marshal Chen.
+ */
+
+package com.fss.common.uiModule.circularProgressButton;
+
+interface OnAnimationEndListener {
+
+    public void onAnimationEnd();
+}
