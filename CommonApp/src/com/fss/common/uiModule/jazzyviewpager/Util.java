7325945aaf799c901a1921431c5c507ebaa5e8cb
marshalchen <cymcsg@gmail.com>
2014-08-18 15:06:11 +0800

    add jazzy viewpager

---
+++ CommonApp/src/com/fss/common/uiModule/jazzyviewpager/Util.java
@@ -0,0 +1,12 @@
+package com.fss.common.uiModule.jazzyviewpager;
+
+import android.content.res.Resources;
+import android.util.TypedValue;
+
+public class Util {
+
+	public static int dpToPx(Resources res, int dp) {
+		return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, res.getDisplayMetrics());
+	}
+
+}
