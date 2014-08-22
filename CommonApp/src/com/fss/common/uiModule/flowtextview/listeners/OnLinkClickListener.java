c4a69381dd928bdfc7e51103dc9f8539bb1abaf1
marshalchen <cymcsg@gmail.com>
2014-08-22 09:47:29 +0800

    add flow text view

---
+++ CommonApp/src/com/fss/common/uiModule/flowtextview/listeners/OnLinkClickListener.java
@@ -0,0 +1,8 @@
+package com.fss.common.uiModule.flowtextview.listeners;
+
+/**
+* Created by Dean on 24/06/2014.
+*/
+public interface OnLinkClickListener {
+    public void onLinkClick(String url);
+}
---
+++ CommonApp/src/com/fss/common/uiModule/flowtextview/models/Line.java
@@ -0,0 +1,9 @@
+package com.fss.common.uiModule.flowtextview.models;
+
+/**
+* Created by Dean on 24/06/2014.
+*/
+public class Line {
+    public float leftBound;
+    public float rightBound;
+}
