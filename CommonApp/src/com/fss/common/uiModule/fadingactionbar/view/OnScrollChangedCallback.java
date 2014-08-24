7a3b2618707d5bc117296164618ef8195af3a0c1
marshalchen <cymcsg@gmail.com>
2014-08-24 19:55:51 +0800

    add fading actionbar

---
+++ CommonApp/src/com/fss/common/uiModule/fadingactionbar/view/OnScrollChangedCallback.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2014. Marshal Chen.
+ */
+
+package com.fss.common.uiModule.fadingactionbar.view;
+
+public interface OnScrollChangedCallback {
+    void onScroll(int l, int t);
+}
