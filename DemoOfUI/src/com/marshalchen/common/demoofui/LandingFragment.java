dc0a99a67fe4ba27ea4d104ec905c0d05798e72f
marshalchen <cymcsg@gmail.com>
2014-09-15 17:52:46 +0800

    add ultimatelistview demo

--- DemoOfUI/src/com/marshalchen/common/demoofui/LandingFragment.java
+++ DemoOfUI/src/com/marshalchen/common/demoofui/LandingFragment.java
@@ -406,6 +406,11 @@ public class LandingFragment extends Fragment {
         map.put("detail", "try");
         map.put("subname", "Layout");
         enhanceList.add(map);
+        map = new HashMap<>();
+        map.put("name", "UltimateListview");
+        map.put("detail", "try");
+        map.put("subname", "Listview");
+        enhanceList.add(map);
 //        map = new HashMap<>();
 //        map.put("name", "ResideMenu");
 //        map.put("detail", "try");
@@ -608,6 +613,9 @@ public class LandingFragment extends Fragment {
                         break;
                     case 58:
                         BasicUtils.sendIntent(getActivity(), SwipeRefreshLayoutDemo.class);
+                        break;
+                    case 59:
+                        BasicUtils.sendIntent(getActivity(), UltimateListviewActivity.class);
                         break;
 
 
