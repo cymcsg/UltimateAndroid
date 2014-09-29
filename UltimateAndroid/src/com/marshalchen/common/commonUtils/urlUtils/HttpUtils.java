e2c303e37349c8c115fde4a8e4211db7b0024c65
marshalchen <cymcsg@gmail.com>
2014-09-29 15:23:54 +0800

    add set utf-8 of http upload

--- UltimateAndroid/src/com/marshalchen/common/commonUtils/urlUtils/HttpUtils.java
+++ UltimateAndroid/src/com/marshalchen/common/commonUtils/urlUtils/HttpUtils.java
@@ -9,6 +9,7 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.conn.ConnectTimeoutException;
+import org.apache.http.entity.ContentType;
 import org.apache.http.entity.mime.HttpMultipartMode;
 import org.apache.http.entity.mime.MultipartEntity;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
@@ -22,6 +23,7 @@ import org.apache.http.util.EntityUtils;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -239,7 +241,8 @@ public class HttpUtils {
 
             if (BasicUtils.judgeNotNull(paramsList)) {
                 for (NameValuePair nameValuePair : paramsList) {
-                    entityBuilder.addTextBody(nameValuePair.getName(), nameValuePair.getValue());
+                    entityBuilder.addTextBody(nameValuePair.getName(), nameValuePair.getValue(),
+                            ContentType.create(HTTP.PLAIN_TEXT_TYPE, HTTP.UTF_8));
                 }
             }
 
