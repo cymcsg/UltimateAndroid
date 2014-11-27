3e2170269c5480d3622ca329da4adfc9bfaa4cf0
marshalchen <cymcsg@gmail.com>
2014-11-27 09:44:47 +0800

    add material_tab

---
+++ UltimateAndroidGradle/ultimateandroidui/src/main/java/com/marshalchen/common/uimodule/materialtabs/MaterialTabListener.java
@@ -0,0 +1,9 @@
+package com.marshalchen.common.uimodule.materialtabs;
+
+public interface MaterialTabListener {
+	public void onTabSelected(MaterialTab tab);
+	
+	public void onTabReselected(MaterialTab tab);
+	
+	public void onTabUnselected(MaterialTab tab);
+}
