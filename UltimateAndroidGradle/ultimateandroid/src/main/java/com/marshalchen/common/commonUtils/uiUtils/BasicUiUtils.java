55bcce7b5421361bb91d8c3ab1705febe9a562a8
marshalchen <cymcsg@gmail.com>
2014-12-23 16:53:28 +0800

    update basic ui utils

--- UltimateAndroidGradle/ultimateandroid/src/main/java/com/marshalchen/common/commonUtils/uiUtils/BasicUiUtils.java
+++ UltimateAndroidGradle/ultimateandroid/src/main/java/com/marshalchen/common/commonUtils/uiUtils/BasicUiUtils.java
@@ -3,6 +3,7 @@ package com.marshalchen.common.commonUtils.uiUtils;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
+import android.util.TypedValue;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
@@ -12,18 +13,21 @@ import android.widget.RelativeLayout;
 
 /**
  * Some method help do some UI works
- *
- *
  */
 public class BasicUiUtils {
-    public static void hiddenKeyboard(Class className, Context context, Activity activity) {
+    /**
+     * Hide soft keyboard method.
+     * @param context
+     * @param activity
+     */
+    public static void hiddenKeyboard(Context context, Activity activity) {
         try {
             InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
 
             if (activity.getCurrentFocus() != null) {
                 if (activity.getCurrentFocus().getWindowToken() != null) {
                     imm.hideSoftInputFromWindow(activity
-                            .getCurrentFocus().getWindowToken(),
+                                    .getCurrentFocus().getWindowToken(),
                             InputMethodManager.HIDE_NOT_ALWAYS);
                 }
             }
@@ -33,39 +37,66 @@ public class BasicUiUtils {
 
     }
 
-    public static void hiddenKeyBoardByClick(Class className, Context context, Activity activity, MotionEvent event) {
-        if (event.getAction() == MotionEvent.ACTION_DOWN) {
-            BasicUiUtils.hiddenKeyboard(className, context, activity);
+    /**
+     * Hide soft keyboard by click。
+     * @param context
+     * @param activity
+     * @param motionEvent Judge by motion event
+     */
+    public static void hiddenKeyBoardByClick(Context context, Activity activity, MotionEvent motionEvent) {
+        if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+            BasicUiUtils.hiddenKeyboard(context, activity);
         }
     }
 
-
+    /**
+     * Converte dp to pixels
+     * @param context
+     * @param dpValue
+     * @return pixels
+     */
     public static int dip2px(Context context, float dpValue) {
-        final float scale = context.getResources().getDisplayMetrics().density;
-        return (int) (dpValue * scale + 0.5f);
+        return (int) Math.ceil(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dpValue, context.getResources().getDisplayMetrics()));
     }
 
-
+    /**
+     * Converting pixels to dp
+     * @param context
+     * @param pxValue
+     * @return dp
+     */
     public static int px2dip(Context context, float pxValue) {
         final float scale = context.getResources().getDisplayMetrics().density;
         return (int) (pxValue / scale + 0.5f);
     }
 
+    /**
+     * Converte dp to pixels
+     * @param context
+     * @param dpValue
+     * @return pixels
+     */
+    public static float dp2px(Context context, float dpValue) {
+        float scale = context.getResources().getDisplayMetrics().density;
+        return (int) Math.ceil(dpValue * scale);
+    }
 
-
+    /**
+     * Expand a view which has already collapsed
+     * @param v
+     */
     public static void expandViews(final View v) {
         v.measure(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
         final int targtetHeight = v.getMeasuredHeight();
 
         v.getLayoutParams().height = 0;
         v.setVisibility(View.VISIBLE);
-        Animation a = new Animation()
-        {
+        Animation a = new Animation() {
             @Override
             protected void applyTransformation(float interpolatedTime, Transformation t) {
                 v.getLayoutParams().height = interpolatedTime == 1
                         ? RelativeLayout.LayoutParams.WRAP_CONTENT
-                        : (int)(targtetHeight * interpolatedTime);
+                        : (int) (targtetHeight * interpolatedTime);
                 v.requestLayout();
             }
 
@@ -76,21 +107,25 @@ public class BasicUiUtils {
         };
 
         // 1dp/ms
-        a.setDuration((int)(targtetHeight / v.getContext().getResources().getDisplayMetrics().density));
+        a.setDuration((int) (targtetHeight / v.getContext().getResources().getDisplayMetrics().density));
         v.startAnimation(a);
     }
 
+
+    /**
+     * Collapse a view which has already expanded
+     * @param v
+     */
     public static void collapseViews(final View v) {
         final int initialHeight = v.getMeasuredHeight();
 
-        Animation a = new Animation()
-        {
+        Animation a = new Animation() {
             @Override
             protected void applyTransformation(float interpolatedTime, Transformation t) {
-                if(interpolatedTime == 1){
+                if (interpolatedTime == 1) {
                     v.setVisibility(View.GONE);
-                }else{
-                    v.getLayoutParams().height = initialHeight - (int)(initialHeight * interpolatedTime);
+                } else {
+                    v.getLayoutParams().height = initialHeight - (int) (initialHeight * interpolatedTime);
                     v.requestLayout();
                 }
             }
@@ -102,12 +137,12 @@ public class BasicUiUtils {
         };
 
         // 1dp/ms
-        a.setDuration((int)(initialHeight / v.getContext().getResources().getDisplayMetrics().density)*1);
+        a.setDuration((int) (initialHeight / v.getContext().getResources().getDisplayMetrics().density) * 1);
         v.startAnimation(a);
     }
 
     /**
-     * Pop a simple alertdialog
+     * Pop a simple alertdialog which only shows title,message and "OK" button
      *
      * @param context
      * @param title
@@ -121,7 +156,7 @@ public class BasicUiUtils {
     }
 
     /**
-     * Pop a simple alertdialog
+     * Pop a simple alertdialog which only shows title,message and "OK" button
      *
      * @param context
      * @param title
