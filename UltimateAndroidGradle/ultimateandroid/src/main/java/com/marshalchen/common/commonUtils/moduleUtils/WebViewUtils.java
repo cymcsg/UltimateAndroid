bcf9e054c042c0f6b55fed79224d8f82b0b8b5f6
marshalchen <cymcsg@gmail.com>
2015-01-03 13:29:51 +0800

    Update WebviewUtils

--- UltimateAndroidGradle/ultimateandroid/src/main/java/com/marshalchen/common/commonUtils/moduleUtils/WebViewUtils.java
+++ UltimateAndroidGradle/ultimateandroid/src/main/java/com/marshalchen/common/commonUtils/moduleUtils/WebViewUtils.java
@@ -5,30 +5,17 @@ import android.webkit.*;
 import com.marshalchen.common.commonUtils.logUtils.Logs;
 
 /**
- * Some default value of webview
- * User: cym
- * Date: 13-10-31
+ * Some utils of WebView
  */
-//WebViewUtils
+
 public class WebViewUtils {
-    private static WebView webView;
 
-    public static WebView getCustomWebView(Context context, String appCacheDir) {
-        webView = new WebView(context);
-
-        WebSettings wSet = webView.getSettings();
-        // wSet.setAppCacheMaxSize();
-        wSet.setJavaScriptEnabled(true);
-        wSet.setDomStorageEnabled(true);
-        // String appCacheDir = this.getActivity().getApplicationContext().getDir("cache", Context.MODE_PRIVATE).getPath();
-        wSet.setAppCachePath(appCacheDir);
-        wSet.setAllowFileAccess(true);
-        wSet.setAppCacheEnabled(true);
-        return webView;
-    }
-
-    ;
-
+    /**
+     * A webview setting which enable JavaScript ,DomStorage and file access.
+     * @param webView
+     * @param appCacheDir
+     * @return
+     */
     public static WebSettings getWebSettings(WebView webView, String appCacheDir) {
         WebSettings wSet = webView.getSettings();
         // wSet.setAppCacheMaxSize();
@@ -43,6 +30,13 @@ public class WebViewUtils {
 
     }
 
+    /**
+     * @deprecated
+     * Forces sync manager to sync now
+     * @param context
+     * @param domainNameUrl
+     * @param strings
+     */
     public static void syncCookie(Context context, String domainNameUrl, String... strings) {
         CookieSyncManager.createInstance(context);
         //CookieSyncManager.getInstance().startSync();
@@ -55,13 +49,22 @@ public class WebViewUtils {
         }
         Logs.d(cookieManager.getCookie(domainNameUrl));
         CookieSyncManager.getInstance().sync();
-        //    CookieSyncManager.getInstance().stopSync();
     }
 
+    /**
+     * Get a historical list of webview
+     * @param webView
+     * @return
+     */
     public static WebBackForwardList getHistoricalList(WebView webView) {
         return webView.copyBackForwardList();
     }
 
+    /**
+     * Get the latest url of webview
+     * @param webView
+     * @return
+     */
     public static String getHistoricalUrl(WebView webView) {
         WebBackForwardList webBackForwardList = getHistoricalList(webView);
         return webBackForwardList.getItemAtIndex(webBackForwardList.getCurrentIndex() - 1).getUrl();
