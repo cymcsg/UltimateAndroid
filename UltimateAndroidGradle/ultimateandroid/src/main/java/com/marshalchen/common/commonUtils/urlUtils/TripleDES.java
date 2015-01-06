83ad49980d3a9adb9d98213ff0bd5e4aad46a741
marshalchen <cymcsg@gmail.com>
2015-01-06 14:38:34 +0800

    add CryptographyUtils

--- UltimateAndroidGradle/ultimateandroid/src/main/java/com/marshalchen/common/commonUtils/urlUtils/TripleDES.java
+++ UltimateAndroidGradle/ultimateandroid/src/main/java/com/marshalchen/common/commonUtils/urlUtils/TripleDES.java
@@ -207,43 +207,7 @@ public class TripleDES {
         return new String(plainText, "UTF-8");
     }
 
-    /**
-     * Revert byte to hex
-     *
-     * @param b
-     * @return
-     */
-    public static String byte2hex(byte[] b) {
-        String hs = "";
-        String stmp = "";
-        for (int n = 0; n < b.length; n++) {
-            stmp = Integer.toHexString(b[n] & 0xFF);
-            if (stmp.length() == 1)
-                hs += ("0" + stmp);
-            else
-                hs += stmp;
-        }
-        return hs.toUpperCase();
-    }
 
-    /**
-     * Revert hex to byte
-     *
-     * @param b
-     * @return
-     */
-    public static byte[] hex2byte(byte[] b) {
-        if ((b.length % 2) != 0)
-            throw new IllegalArgumentException("wrong index");
-
-        byte[] b2 = new byte[b.length / 2];
-
-        for (int n = 0; n < b.length; n += 2) {
-            String item = new String(b, n, 2);
-            b2[n / 2] = (byte) Integer.parseInt(item, 16);
-        }
-        return b2;
-    }
 
     public static String getToken() {
         return token;
