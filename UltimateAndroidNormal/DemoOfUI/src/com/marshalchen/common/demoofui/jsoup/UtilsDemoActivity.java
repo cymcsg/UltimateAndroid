126727087d0ddecf744856f2c39b7d809af864cc
Yu Lin <yu.lin.86@gmail.com>
2015-05-29 20:55:37 -0500

    Convert Title AsyncTask to IntentService

--- UltimateAndroidNormal/DemoOfUI/src/com/marshalchen/common/demoofui/jsoup/UtilsDemoActivity.java
+++ UltimateAndroidNormal/DemoOfUI/src/com/marshalchen/common/demoofui/jsoup/UtilsDemoActivity.java
@@ -1,8 +1,14 @@
 package com.marshalchen.common.demoofui.jsoup;
 
 import android.app.Activity;
+import android.app.IntentService;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
 import android.widget.TextView;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -17,43 +23,36 @@ import java.io.IOException;
  * Created by cym on 14-7-19.
  */
 public class UtilsDemoActivity extends Activity {
+    private static final String TITLE_FILTER = "UtilsDemoActivity_titleReceiver";
     String url = "http://blog.marshalchen.com/";
     @InjectView(R.id.jsoupTextView)
     TextView mJsoupTextView;
+    Title titleReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.jsoup_activity);
         ButterKnife.inject(this);
-        new Title().execute();
+        titleReceiver = new Title();
+        LocalBroadcastManager.getInstance(this).registerReceiver(titleReceiver, new IntentFilter(TITLE_FILTER));
+        Intent title = new Intent(this, TitleService.class);
+        title.putExtra("url", url);
+        this.startService(title);
         mJsoupTextView.setText(UtilsDemo.TestJsoup());
     }
 
-    // Title AsyncTask
-    private class Title extends AsyncTask<Void, Void, Void> {
-        String title;
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (titleReceiver != null)
+            LocalBroadcastManager.getInstance(this).unregisterReceiver(titleReceiver);
+    }
 
+    private class Title extends BroadcastReceiver {
         @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-        }
-
-        @Override
-        protected Void doInBackground(Void... params) {
-            try {
-                // Connect to the web site
-                Document document = Jsoup.connect(url).get();
-                // Get the html document title
-                title = document.title();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void result) {
+        public void onReceive(Context receiverContext, Intent receiverIntent) {
+            String title = receiverIntent.getStringExtra("title");
             // Set title into TextView
             Logs.d("title---" + title);
 //            TextView txttitle = (TextView) findViewById(R.id.titletxt);
@@ -62,5 +61,28 @@ public class UtilsDemoActivity extends Activity {
         }
     }
 
+    // Title IntentService
+    public static class TitleService extends IntentService {
+        private String url;
+        private String title;
 
+        public TitleService() {
+            super("TitleService");
+        }
+
+        public void onHandleIntent(Intent intent) {
+            this.url = intent.getStringExtra("url");
+            try {
+                // Connect to the web site
+                Document document = Jsoup.connect(url).get();
+                // Get the html document title
+                title = document.title();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Intent resultIntent = new Intent(TITLE_FILTER);
+            resultIntent.putExtra("title", title);
+            LocalBroadcastManager.getInstance(this).sendBroadcast(resultIntent);
+        }
+    }
 }
