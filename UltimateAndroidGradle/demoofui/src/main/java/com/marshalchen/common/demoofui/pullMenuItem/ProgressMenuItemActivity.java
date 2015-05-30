a847e2d55d1cedee3a4403325aaa55d87101a8af
Yu Lin <yu.lin.86@gmail.com>
2015-05-29 20:31:00 -0500

    convert ReloadAsyncTask to IntentService

--- UltimateAndroidGradle/demoofui/src/main/java/com/marshalchen/common/demoofui/pullMenuItem/ProgressMenuItemActivity.java
+++ UltimateAndroidGradle/demoofui/src/main/java/com/marshalchen/common/demoofui/pullMenuItem/ProgressMenuItemActivity.java
@@ -1,6 +1,9 @@
 package com.marshalchen.common.demoofui.pullMenuItem;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -8,18 +11,32 @@ import android.view.MenuItem;
 import com.marshalchen.common.commonUtils.basicUtils.BasicUtils;
 import com.marshalchen.common.demoofui.HowToUseActivity;
 import com.marshalchen.common.demoofui.R;
+import com.marshalchen.common.demoofui.pullMenuItem.ReloadIntentService;
+import com.marshalchen.common.demoofui.pullMenuItem.ReloadIntentService.ReloadReceiver;
 
 import progress.menu.item.ProgressMenuItemHelper;
 import progress.menu.item.ProgressMenuItemSize;
 
 public class ProgressMenuItemActivity extends ActionBarActivity {
+    static final String RELOAD_FILTER = "ProgressMenuItemActivity_reloadReceiver";
 
     private ProgressMenuItemHelper progressHelper;
+    private ReloadReceiver reloadReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.progress_menu_item_activity_main);
+        reloadReceiver = new ReloadReceiver(progressHelper);
+        LocalBroadcastManager.getInstance(this)
+            .registerReceiver(reloadReceiver, new IntentFilter(RELOAD_FILTER));
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (reloadReceiver != null)
+            LocalBroadcastManager.getInstance(this).unregisterReceiver(reloadReceiver);
     }
 
     @Override
@@ -33,7 +50,9 @@ public class ProgressMenuItemActivity extends ActionBarActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_refresh:
-                new ReloadAsyncTask(progressHelper).execute();
+                Intent reloadService = new Intent(this, ReloadIntentService.class);
+                reloadReceiver.startProgress();
+                this.startService(reloadService);
                 return true;
             case R.id.howToUse:
                 BasicUtils.sendIntent(this, HowToUseActivity.class, "data", "ProgressMenuItemActivity");
